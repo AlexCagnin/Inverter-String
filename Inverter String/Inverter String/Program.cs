4e5748ce9b056dd1a0af40095d095c9b1ce16297
Alex <84486142+AlexCagnin@users.noreply.github.com>
2023-02-27 20:28:57 -0300

    Inverter String

---
+++ Inverter String/Inverter String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inverter_String {
+    internal class Program {
+        private static void Main(string[] args) {
+
+
+            Console.Write("Digite um nome: ");
+            string nome = Console.ReadLine();
+
+            char[] caracteres = nome.ToCharArray();
+            int tamanho = caracteres.Length;
+
+            for (int i = 0; i < tamanho / 2; i++) {
+                char temp = caracteres[i];
+                caracteres[i] = caracteres[tamanho - 1 - i];
+                caracteres[tamanho - 1 - i] = temp;
+            }
+
+            string nomeInvertido = new string(caracteres);
+            Console.WriteLine("Nome invertido: " + nomeInvertido);
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+
+    }
+
